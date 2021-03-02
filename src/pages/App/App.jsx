7a144ef6f784657d076cfaa4ac9b7616e9d41983
Benjamin Pitts <benjaminpitts@Benjamins-MacBook-Pro.local>
2021-03-02 11:33:28 -0700

    first commit

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -28,22 +28,22 @@ class App extends Component {
   render() {
     return (
       <div>
-        <NavBar 
-        user={this.state.user} 
+        <NavBar
+        user={this.state.user}
         handleLogout={this.handleLogout}
         />
         <Switch>
           <Route exact path='/' render={() =>
-           <div>Hello World!</div> 
+           <div>Hello World! This is a test app with auth!</div> 
           }/>
-          <Route exact path='/signup' render={({ history }) => 
+          <Route exact path='/signup' render={({ history }) =>
             <SignupPage
               history={history}
               handleSignupOrLogin={this.handleSignupOrLogin}
-              
+
             />
           }/>
-          <Route exact path='/login' render={({history}) => 
+          <Route exact path='/login' render={({history}) =>
             <LoginPage
               history={history}
               handleSignupOrLogin={this.handleSignupOrLogin}
